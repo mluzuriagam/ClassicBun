7ccca03fdbe85b1d0998058b32ea0039bcb6c798
mluzuriagam <117092859+mluzuriagam@users.noreply.github.com>
2023-01-17 15:49:45 -0500

    ec update

--- main.py
+++ main.py
@@ -43,6 +43,11 @@ def index():
     return render_template("index.html", success=True)
 
 
+@app.route("/ec/")
+def index3():
+    return render_template("index.html", success=True)
+
+
 @app.route("/ec")
 def index3():
     return render_template("index.html", success=True)
